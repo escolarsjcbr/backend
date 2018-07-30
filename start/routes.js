eeb021773073a726fdd7958d3d3ca4050740fd29
Lennon S Bueno <lennonbueno@sistemaathos.com.br>
2018-07-30 00:39:14 -0300

    inicial commit

---
+++ start/routes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+const Route = use("Route");
+
+Route.get('/', () => 'Hello world');
+
+Route.post("/register", "AuthController.register");
+Route.post("/authenticate", "AuthController.authenticate");
+
+Route.get("/app", "AppController.index").middleware(["auth"]);
